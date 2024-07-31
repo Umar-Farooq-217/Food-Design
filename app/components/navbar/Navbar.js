249ae0129d87f768b8ddfb60a4259b8f82a0c37d
Umar Farooq <uf29664@gmail.com>
2024-07-31 12:43:43 +0500

    food

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,7 +1,10 @@
+'use client'
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { IoMenu } from "react-icons/io5";
+import { useState } from 'react';
+import { MdClose } from "react-icons/md";
 
 
 export default function Navbar() {
@@ -13,29 +16,33 @@ export default function Navbar() {
     {name:'Contact Us',link:'/'}
   ]
 
+  const [open,setOpen]= useState(false)
   return (
-    <div className='lg:flex items-center justify-between md:block xl:px-48 lg:px-28 lg:bg-red-400 md:bg-green-400 w-[100%]'>
-      <div>
-      <IoMenu className='lg:invisible md:visible text-4xl absolute top-9 right-5'/>
+    <div className='lg:flex items-center justify-between md:block xl:px-48 lg:px-28 '>
+      <div onClick={()=>setOpen(!open)} className='lg:invisible md:visible text-4xl absolute top-9 right-5'>
+        {open?
+      <MdClose className=' text-4xl '/>:
+      <IoMenu className=' text-4xl '/>
+}
       </div>
         <div>
             <Image className='w-[106px] h-[86px] sm:pt-3 ' src='/logo.png' width={80} height={80} alt='logo' />
         </div>
         <div>
-          <ul className='lg:flex items-center justify-center md:block'>
+          <ul className={`lg:flex items-center justify-center md:block md:pt-4 sm:pt-4 lg:pt-0 absolute lg:static lg:z-auto  transition-all duration-500 ease-in ${open? 'top-20 md:left-[45%] sm:left-32':'top-[-450px] md:left-[45%] sm:left-32'} `}>
             {
               links.map((link,i)=>{
                 return(
 
-                  <li className='text-xl font-semibold px-3' key={i}><Link href={link.link}>{link.name}</Link></li>
+                  <li className='text-xl font-semibold px-3 md:py-4 sm:py-4 lg:py-0 md:text-center sm:text-center' key={i}><Link href={link.link}>{link.name}</Link></li>
                 )
               })
             }
           </ul>
 
         </div>
-        <div>
-          <button className='bg-[rgb(236,8,8)] py-3 px-7 rounded-3xl hover:scale-105 text-white font-bold'><Link href='/'>Order Now</Link></button>
+        <div className={`absolute lg:static lg:z-auto transition-all duration-500 ease-in ${open ? 'top-96 md:left-[45%] sm:left-32' : 'top-[-550px] md:left-[45%] sm:left-32'} `}>
+          <button className='bg-[rgb(236,8,8)] py-3 px-7 rounded-3xl hover:scale-105 text-white font-bold sm:my-4 sm:flex sm:mx-auto'><Link href='/'>Order Now</Link></button>
         </div>
 
       
--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar";
-
+import './globals.css'
 export default function Home() {
   return (
   <div>
     <Navbar/>
+    <div className='first'>
+      <div className='bg-gray-900 w-[100%] h-[90vh] opacity-60'>
+
+      </div>
+
+    </div>
   </div>
   );
 }
