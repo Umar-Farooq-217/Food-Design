dad7886bae9323ac05f5b868ec0486c53526fb84
Umar Farooq <uf29664@gmail.com>
2024-08-02 18:19:58 +0500

    food

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -18,7 +18,7 @@ export default function Navbar() {
 
   const [open,setOpen]= useState(false)
   return (
-    <div className='lg:flex items-center justify-between md:block xl:px-48 lg:px-28 '>
+    <div className='lg:flex items-center justify-between md:block xl:px-48 lg:px-28 reletive z-10 '>
       <div onClick={()=>setOpen(!open)} className='lg:invisible md:visible text-4xl absolute top-9 right-5'>
         {open?
       <MdClose className=' text-4xl '/>:
@@ -29,19 +29,19 @@ export default function Navbar() {
             <Image className='w-[106px] h-[86px] sm:pt-3 ' src='/logo.png' width={80} height={80} alt='logo' />
         </div>
         <div>
-          <ul className={`lg:flex items-center justify-center md:block md:pt-4 sm:pt-4 lg:pt-0 absolute lg:static lg:z-auto  transition-all duration-500 ease-in ${open? 'top-20 md:left-[45%] sm:left-32':'top-[-450px] md:left-[45%] sm:left-32'} `}>
+          <ul className={`z-20 lg:flex items-center justify-center md:block md:pt-4 sm:pt-4 lg:pt-0 absolute lg:static lg:z-auto  transition-all duration-500 ease-in ${open? 'top-20 md:left-[45%] sm:left-24':'top-[-450px] md:left-[45%] sm:left-24'} `}>
             {
               links.map((link,i)=>{
                 return(
 
-                  <li className='text-xl font-semibold px-3 md:py-4 sm:py-4 lg:py-0 md:text-center sm:text-center' key={i}><Link href={link.link}>{link.name}</Link></li>
+                  <li className='text-xl font-semibold px-3 md:py-4 sm:py-4 lg:py-0 md:text-white sm:text-white lg:text-black md:text-center sm:text-center sm:text-whitebg-slate-800 sm:w-56 lg:w-auto md:w-auto shadow-3xl' key={i}><Link href={link.link}>{link.name}</Link></li>
                 )
               })
             }
           </ul>
 
         </div>
-        <div className={`absolute lg:static lg:z-auto transition-all duration-500 ease-in ${open ? 'top-96 md:left-[45%] sm:left-32' : 'top-[-550px] md:left-[45%] sm:left-32'} `}>
+        <div className={`z-20 shadow-3xl absolute lg:static lg:z-auto transition-all duration-500 ease-in ${open ? 'top-96 md:left-[45%] sm:left-32' : 'top-[-550px] md:left-[45%] sm:left-32'} `}>
           <button className='bg-[rgb(236,8,8)] py-3 px-7 rounded-3xl hover:scale-105 text-white font-bold sm:my-4 sm:flex sm:mx-auto'><Link href='/'>Order Now</Link></button>
         </div>
 
--- app/page.js
+++ app/page.js
@@ -1,18 +1,36 @@
 import Image from "next/image";
 import Navbar from "./components/navbar/Navbar";
+import { FaStar } from "react-icons/fa6";
+
 import './globals.css'
 export default function Home() {
   return (
   <div>
     <Navbar/>
-    <div className='first'>
-    
-      {/* <div className='bg-gray-900 w-[100%] h-[90vh] opacity-60'>
-      </div> */}
+    <div className='first md:px-32 sm:px-10 '>
+    <h1 className='md:text-9xl sm:text-5xl  font-bold text-white'>GOURMET FOOD </h1>
+    <h2 className='md:text-9xl sm:text-5xl  font-bold text-red-600'>DELIVERED TO</h2>
+    <h3  className='md:text-9xl sm:text-5xl  font-bold text-white'>YOUR DOORSTEP</h3>
+    <p className='text-3xl text-white font-bold'> Experience the Flavors of Our Signature Dishes, Made with Love <br /> and Fresh Ingredients Without Leaving Your Doorstep</p>
 
+<div className='btns flex space-x-4 w-96'>
+<button className='bg-[rgb(236,8,8)] py-4 px-8 rounded-[30px] hover:scale-105 text-white font-bold sm:my-4 sm:flex sm:mx-auto'>Order Now</button>
+<button className='border-2 border-white py-4 px-8 rounded-[30px] hover:scale-105 text-white font-bold sm:my-4 sm:flex sm:mx-auto'>Order Now</button>
 
-        <h1 className='text-9xl  font-bold text-white'>GOURMET FOOD </h1>
+</div>
+<div className='relative z-10 bg-white rounded-lg flex justify-center items-center w-72 h-72 top-[250px]'>
+  <div>
+    <Image src='/Group 3.svg' width={60} height={60} />
+  </div>
+  <div>
+    <p className='text-gray-700 text-xl font-bold'>Google Rating</p>
+    <h1 className='text-3xl text-[rgb(264,254,0)] font-bold flex'>4.8 <sup><FaStar /></sup><sup><FaStar /></sup>
+    <sup><FaStar /></sup><sup><FaStar /></sup><sup><FaStar /></sup><sup><FaStar /></sup></h1>
+    <p className='text-gray-700 text-xl '>See all our services </p>
 
+  </div>
+
+</div>
 
     </div>
   </div>
