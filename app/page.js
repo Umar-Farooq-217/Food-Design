bc5c65851672f6ab07680af85294a74d0862b054
Umar Farooq <uf29664@gmail.com>
2024-08-06 18:27:26 +0500

    food

--- app/page.js
+++ app/page.js
@@ -3,31 +3,34 @@ import Navbar from "./components/navbar/Navbar";
 import { FaStar } from "react-icons/fa6";
 
 import './globals.css'
+import FirstCard from "./components/firstCard/FirstCard";
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className='first md:px-32 sm:px-10 lg:h-[100vh] md:h-[1400px] sm:h-[1200px] '>
-        <h1 className='lg:text-9xl md:text-8xl sm:text-5xl  font-bold text-white'>GOURMET FOOD </h1>
-        <h2 className='lg:text-9xl md:text-8xl sm:text-5xl  font-bold text-red-600'>DELIVERED TO</h2>
-        <h3 className='lg:text-9xl md:text-8xl sm:text-5xl  font-bold text-white'>YOUR DOORSTEP</h3>
-        <p className='md:text-3xl sm:text-2xl text-white font-bold'> Experience the Flavors of Our Signature Dishes, Made with Love <br /> and Fresh Ingredients Without Leaving Your Doorstep</p>
+      <div className='first md:px-32 sm:px-8  '>
+        <h1 className='lg:text-9xl md:text-8xl sm:text-5xl  font-bold text-white pt-36'>GOURMET FOOD </h1>
+        <h2 className='lg:text-9xl md:text-8xl sm:text-5xl  font-bold text-red-600 pt-8'>DELIVERED TO</h2>
+        <h3 className='lg:text-9xl md:text-8xl sm:text-5xl  font-bold text-white pt-8'>YOUR DOORSTEP</h3>
+        <p className='md:text-3xl sm:text-xl text-white font-bold pt-6'> Experience the Flavors of Our Signature Dishes, Made with Love <br /> and Fresh Ingredients Without Leaving Your Doorstep</p>
 
-        <div className='btns flex space-x-2 md:w-[400px] sm:w-[310px]'>
-          <button className='bg-[rgb(236,8,8)] py-4 px-8 rounded-[30px] hover:scale-105 text-white font-bold sm:my-4 sm:flex sm:mx-auto'>Order Now</button>
+        <div className=' flex w-[320px]  md:space-x-1 pt-6 sm:mx-[-15px] flex-wrap  '>
+          <button className=' bg-[rgb(236,8,8)] py-4  px-8 rounded-[30px] hover:scale-105 text-white font-bold sm:my-4 sm:flex sm:mx-auto'>Order Now</button>
           <button className='border-2 border-white py-4 px-8 rounded-[30px] hover:scale-105 text-white font-bold sm:my-4 sm:flex sm:mx-auto cursor-pointer'>See Menu</button>
 
         </div>
-        <div className='relative z-10 bg-white rounded-lg flex justify-betweem items-center md:w-[450px] sm:w-[300px] h-36 top-[300px]'>
+        
+        <div className='mt-8 bg-white rounded-lg flex justify-betweem items-center md:w-[450px] sm:w-[300px] h-36 top-[300px]'>
           <div className='md:ml-6 sm:ml-1'>
             <Image src='/Group 3.svg' width={60} height={60} alt='Rating Icon' />
           </div>
-          <div className='relative bottom-[160px] md:left-12 sm:left-4'>
+          <div className='md:ml-9 sm:ml-3'>
 
-            <p className='text-gray-700 text-xl font-bold '>Google Rating</p>
-            <h1 className='text-3xl text-[rgb(264,254,0)] font-bold flex relative top-28'>4.8  <span className='flex md:ml-2 sm:ml-[5px] mb-2'>
+            <p className='text-gray-700 text-xl font-bold py-2 '>Google Rating</p>
+            <h1 className='text-3xl text-[rgb(264,254,0)] font-bold flex py-2'>4.8  <span className='flex md:ml-2 sm:ml-[5px] mb-2'>
               <FaStar /><FaStar /><FaStar /><FaStar /><FaStar />
             </span></h1>
+            <p className='text-gray-700 text-xl font-bold py-2 '>See Our all services</p>
 
           </div>
 
@@ -39,13 +42,18 @@ export default function Home() {
       {/* 2nd div ----------- */}
 
 
-      <div>
+      <div className='grid grid-cols-2 px-16 gap-5'>
         <div>
-          <h1 className='text-9xl font-bold'>WHO WE ARE & <span>WHAT WE CAN DO</span></h1>
-          <p>At the Red Gourmet Kitchen, we have put so much love and care into every recipe to give you the very best gourmet cooking in Abuja. 
+          <h1 className='text-8xl font-semibold  pt-12'>WHO WE ARE & <br /><span className='text-[rgb(236,8,8)]'>WHAT WE  DO</span></h1>
+          <p className='text-3xl pr-5 pt-8 font-semibold '>At the Red Gourmet Kitchen, we have put so much love and care into every recipe to give you the very best gourmet cooking in Abuja. 
             All our ingredients are sourced from local farmers and markets. Great produce, great food, great vibes.</p>
+          <FirstCard/>
         </div>
-        <div></div>
+        <div className='flex justify-center flex-wrap gap-5 pt-24'>
+          <Image className='' src='/burger.png' width={395} height={400} alt='burger Image' />
+          <Image className='' src='/tika.png' width={455} height={430} alt='burger Image' />
+          <Image className='' src='/shawarma.png' width={419} height={389} alt='burger Image' />
+        </div>
 
       </div>
     </div>
